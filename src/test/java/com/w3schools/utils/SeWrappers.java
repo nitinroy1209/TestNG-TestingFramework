c06add8dc4472e1ce4bed6d9f9143f700b9b2846
nitin <nitinroy1209@gmail.com>
2023-08-19 18:20:14 +0530

    frameworkreport

--- src/test/java/com/w3schools/utils/SeWrappers.java
+++ src/test/java/com/w3schools/utils/SeWrappers.java
@@ -1,8 +1,10 @@
 package com.w3schools.utils;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,9 +12,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /*
  * This class will contain all the wrapper methods for built in selenium methods
@@ -20,7 +28,7 @@ import org.openqa.selenium.JavascriptExecutor;
 
 public class SeWrappers {
 	
-	public WebDriver driver=null;
+	public static WebDriver driver=null;
 	
 	//Launch browser for the specified url
 	//parameter-> url
@@ -42,6 +50,7 @@ public class SeWrappers {
 	}
 	
 	//method to close all the browsers
+	@AfterMethod
 	public void closeAllBrowsers()
 	{
 		try
@@ -68,8 +77,8 @@ public class SeWrappers {
 			ex.printStackTrace();
 		}
 	}
-	
-	public void typeText(WebElement ele, String text)//it takes two argument webelement whwre i need to type and what text i want to type
+//sendkeys	
+	public void typeText(WebElement ele, String text)
 	{
 		try
 		{
@@ -79,11 +88,12 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in sendkeys method");
 			ex.printStackTrace();
 		}
 		
 	}
-	
+	//click 
 	public void click(WebElement ele)
 	{
 		try
@@ -93,10 +103,50 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in click option");
 			ex.printStackTrace();
 		}
 	}
-	
+	//navigate forward
+	public void navigateforward()
+	{
+		try
+		{
+			driver.navigate().forward();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in navigate forward");
+			ex.printStackTrace();
+		}
+	}
+	//navigate back
+	public void navigateBack()
+	{
+		try
+		{
+			driver.navigate().back();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in navigate back");
+			ex.printStackTrace();
+		}
+	}
+	//navigate refresh
+	public void navigateRefresh()
+	{
+		try
+		{
+			driver.navigate().refresh();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in navigate refresh");
+			ex.printStackTrace();
+		}
+	}
+	//explicit wait
 	public void waitForMe(WebElement ele, int timeout)
 	{
 		try
@@ -107,18 +157,24 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in explicit wait");
 			ex.printStackTrace();
 		}
 	}
-	
+	//fluent wait
 	public void pollingWait(WebElement ele, int timeout, int pollingFrequency)
 	{
 		try
 		{
+			FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+					.withTimeout(Duration.ofSeconds(timeout)) 
+					.pollingEvery(Duration.ofSeconds(pollingFrequency));
+			wait.until(ExpectedConditions.visibilityOf(ele));
 			
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in fluent wait");
 			ex.printStackTrace();
 		}
 	}
@@ -133,6 +189,7 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in selectByVisible text");
 			ex.printStackTrace();
 		}
 		
@@ -148,6 +205,7 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in selectBy index");
 			ex.printStackTrace();
 		}
 	}
@@ -161,6 +219,7 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in selectBy value");
 			ex.printStackTrace();
 		}
 	}
@@ -174,6 +233,7 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in accept alert");
 			ex.printStackTrace();
 		}
 	}
@@ -187,6 +247,7 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in dismiss alert");
 			ex.printStackTrace();
 		}
 	}
@@ -203,6 +264,7 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in getText method");
 			ex.printStackTrace();
 		}
 	}
@@ -218,11 +280,12 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in typeText in alert");
 			ex.printStackTrace();
 		}
 	}
 	
-	//Frames --only for frame input wont be webelement but for select class we need to pass the driver object,need to create object for alert,select class and action..
+	//Frames
 	public void frameByWebElement(WebElement ele)
 	{
 		try
@@ -232,6 +295,7 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in frames");
 			ex.printStackTrace();
 		}
 	}
@@ -245,12 +309,13 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in frameBy index");
 			ex.printStackTrace();
 		}
 	}
 	
-	//FrameByIndex
-		public void frameByIndex(String nameOrId)
+	//FrameByName
+		public void frameByname(String nameOrId)
 		{
 			try
 			{
@@ -259,10 +324,11 @@ public class SeWrappers {
 			}
 			catch(Exception ex)
 			{
+				System.out.println("problem in frameBy name");
 				ex.printStackTrace();
 			}
 		}
-		
+	//isSelected method	
 	public boolean verifySelected(WebElement ele)
 	{
 		boolean retVal=false;
@@ -287,8 +353,54 @@ public class SeWrappers {
 	}
 		
 		//isEnabled
+	public boolean verifyEnabled(WebElement ele)
+	{
+	boolean	retVal=false;
+		try
+		{
+			if(ele.isEnabled())
+			{
+				retVal=true;
+				System.out.println("element is enabled");	
+			}
+			else
+			{
+				retVal=false;
+				System.out.println("element is not enabled");
+			}
+			
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return retVal;
+	}
 	//isDisplayed
-	
+	public boolean verifyDisplayed(WebElement ele)
+	{
+	boolean	retVal=false;
+		try
+		{
+			if(ele.isDisplayed())
+			{
+				retVal=true;
+				System.out.println("element is displayed");	
+			}
+			else
+			{
+				retVal=false;
+				System.out.println("element is not displayed");
+			}
+			
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return retVal;
+	}
+	//action-click
 	public void actionClick(WebElement ele)
 	{
 		try
@@ -298,10 +410,11 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in action click");
 			ex.printStackTrace();
 		}
 	}
-	
+//right click	
 	public void rightClick(WebElement ele)
 	{
 		try
@@ -311,10 +424,11 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in rightclick method");
 			ex.printStackTrace();
 		}
 	}
-	
+	//dragAndDrop
 	public void dragAndDrop(WebElement source, WebElement target)
 	{
 		try
@@ -324,15 +438,69 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in drag and drop method");
 			ex.printStackTrace();
 		}
 	}
 	
 	//clickAndHold
+	public void clickAndhold(WebElement ele)
+	{
+		try
+		{
+			Actions act=new Actions(driver);
+			act.clickAndHold(ele).build().perform();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in click And Hold method");
+			ex.printStackTrace();
+		}
+	}
 	//docubleClick
+	public void doubleClick(WebElement ele)
+	{
+		try
+		{
+			Actions act=new Actions(driver);
+			act.doubleClick(ele).build().perform();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in doubleclick method");
+			ex.printStackTrace();
+		}
+	}
 	//moveToElement
+	public void MoveToelement(WebElement ele)
+	{
+		try
+		{
+			Actions act=new Actions(driver);
+			act.moveToElement(ele).build().perform();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in moveToElement method");
+			ex.printStackTrace();
+		}
+	}
 	//Keyboard actions
+	public void keyboardActions(WebElement ele,String text)
+	{
+		try
+		{
+			Actions act=new Actions(driver);
+			act.keyDown(ele,Keys.SHIFT).sendKeys(text).perform();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in keyBoard actions");
+			ex.printStackTrace();
+		}
+	}
 	
+	//javascriptexecutor--vertical scroll
 	public void jsVerticalScroll(int y)
 	{
 		try
@@ -343,10 +511,11 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in javascript executor vertical scroll");
 			ex.printStackTrace();
 		}
 	}
-	
+//horizontalscroll	
 	public void jsHorizontalScroll(int x)
 	{
 		try
@@ -357,15 +526,98 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in javascript executor horizontal scroll");
+			ex.printStackTrace();
+		}
+	}
+//scroll down to the bottom of the page
+	public void jsscrollDownBottom()
+	{
+	try
+	{
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
+		
+	}
+	catch(Exception ex)
+	{
+		System.out.println("problem in  scroll down to the bottom of the page");
+		ex.printStackTrace();
+	}
+	}
+//scroll to the top of the page
+	public void jsScrollUp()
+	{
+		try
+		{
+			JavascriptExecutor js = (JavascriptExecutor)driver;
+			js.executeScript("window.scrollTo(0,0);");
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in  scroll  to the top of the page");
+			ex.printStackTrace();
+		}
+	}
+	//jsClick
+	public void jsClick(WebElement ele)
+	{
+		try
+		{
+			JavascriptExecutor js=(JavascriptExecutor)driver;
+			js.executeScript("arguments[0].click();",ele);
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in javascript executor click method");
+			ex.printStackTrace();
+		}
+	}
+	//jstitle
+	public void jsTitle()
+	{
+		try
+		{
+			JavascriptExecutor js=(JavascriptExecutor)driver;
+			System.out.println(js.executeScript("return document.title();"));
+			
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in javascript executor title method");
+			ex.printStackTrace();
+		}
+	}
+	//typeTextwithout snedkeys
+	public void typewithoutSendKeys(WebElement ele,String text)
+	{
+		try
+		{
+			JavascriptExecutor js=(JavascriptExecutor)driver;
+			js.executeScript("arguments[0].value='+text+';",ele);
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in javascriptexecutor typewithout sendkeys method");
+			ex.printStackTrace();
+		}
+	}
+	//scrollintoview
+	public void scrollintoView(WebElement ele)
+	{
+		try
+		{
+			JavascriptExecutor js=(JavascriptExecutor)driver;
+			js.executeScript("arguments[0].scrollIntoView(true);",ele);
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in scrollIntoview method");
 			ex.printStackTrace();
 		}
 	}
 	
-	
-	//jsClick
-	//jstitle
-	//typeTextwithout snedkeys
-	//scrollintoview
+		//window handling
 	
 	public void switchWindows()
 	{
@@ -385,11 +637,54 @@ public class SeWrappers {
 		}
 		catch(Exception ex)
 		{
+			System.out.println("problem in window handling");
+			ex.printStackTrace();
+		}
+	}
+	//get Title
+	public String getTitle()
+	{
+		String title="";
+		try
+		{
+		title=driver.getTitle();
+		System.out.println(title);
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in getTitle method");
+			ex.printStackTrace();
+		}
+		return title;
+	}
+	//getCurrentUrl
+	public void getCurrentUrl()
+	{
+		try
+		{
+			String currentUrl=driver.getCurrentUrl();
+			System.out.println(currentUrl);
+		}
+		catch(Exception ex)
+		{
+			System.out.println("problem in getCurrent Url method");
 			ex.printStackTrace();
 		}
 	}
 	
-	//getCurrentUrl
+	public void screenshot(String screenshotName)
+	{
+		try
+		{
+			File source= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			File dest= new File(System.getProperty("user.dir")+"/screenshots/"+screenshotName+".png");
+			FileUtils.copyFile(source, dest);
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 	
 
 }
